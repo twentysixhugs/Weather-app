ff69de7d17279581cfecfbab37f5e31164d18fba
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-02-19 19:27:02 +0300

    customize the nothing found error

--- src/api_fetcher.js
+++ src/api_fetcher.js
@@ -2,18 +2,18 @@
 // https://api.openweathermap.org/data/2.5/weather?q=London&appid=6275fbc643b73392b948f38f9f200287
 
 async function getWeatherData(location) {
-  try {
-    const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=6275fbc643b73392b948f38f9f200287`,
-      { mode: 'cors' }
-    );
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=6275fbc643b73392b948f38f9f200287`,
+    { mode: 'cors' }
+  );
 
-    if (!response.ok) throw new Error('City not found. Please, try again.');
-    return response.json();
-  } catch (err) {
-    alert(err)
-    return;
-  }
+  if (!response.ok) {
+    const error = new Error('City not found.');
+    error.name = "NothingFound";
+
+    throw error;
+  };
+  return response.json();
 }
 
 export default getWeatherData;
