53f8fef240b4be033511dd6708e0c57dbc8840d8
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-02-19 16:28:53 +0300

    add weather data fetch

--- src/api_fetcher.js
+++ src/api_fetcher.js
@@ -1,2 +1,13 @@
 // 6275fbc643b73392b948f38f9f200287
 // https://api.openweathermap.org/data/2.5/weather?q=London&appid=6275fbc643b73392b948f38f9f200287
+
+async function getWeatherData(location) {
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=6275fbc643b73392b948f38f9f200287`,
+    { mode: 'cors' }
+  );
+
+  return response.json();
+}
+
+export default getWeatherData;
