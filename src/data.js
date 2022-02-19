74c498b0a94da63716ac60f30d34ee503d648e98
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-02-19 17:49:18 +0300

    Add a class and a function that process incoming data
    * The class processed anything not nested and constructs a new object with the specified props
    * The function uses an instance of the class to reduce weather data

---
+++ src/data.js
@@ -0,0 +1,22 @@
+class Data {
+  constructor(...propsAndValues) {
+    propsAndValues.forEach(propAndValue => {
+      this[propAndValue[0]] = propAndValue[1];
+    });
+  }
+}
+
+function getReducedWeatherData(data) {
+  const reducedData = new Data(
+    ['feels_like', data.main.feels_like],
+    ['humidity', data.main.humidity],
+    ['temp', data.main.temp],
+    ['name', data.name],
+    ['country', data.sys.country],
+    ['wind_speed', data.wind.speed],
+  );
+
+  return reducedData;
+}
+
+export default getReducedWeatherData;
