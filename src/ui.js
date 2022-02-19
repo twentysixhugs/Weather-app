a862aa08ff6b5d2e0172a0c6060109cc80793b57
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-02-19 19:25:18 +0300

    add input scanning

--- src/ui.js
+++ src/ui.js
@@ -1,11 +1,19 @@
 const ui = (() => {
   function initSearch(handleSearchSubmit) {
     const search = document.querySelector('.js-search');
-    search.addEventListener('submit', handleSearchSubmit)
+    search.addEventListener('submit', handleSearchSubmit.bind(null, _getInputValue()));
+  }
+
+  function _getInputValue() {
+    return document.querySelector('.js-input').value;
+  }
+
+  function displaySearchResults(results) {
+
   }
 
   return {
-    initSearch
+    initSearch,
   };
 })();
 
