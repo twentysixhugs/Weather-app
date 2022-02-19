8a91ce83f0b32165f02a516e9fae8e5925d56b0f
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-02-19 16:51:54 +0300

    add error handling

--- src/api_fetcher.js
+++ src/api_fetcher.js
@@ -2,12 +2,18 @@
 // https://api.openweathermap.org/data/2.5/weather?q=London&appid=6275fbc643b73392b948f38f9f200287
 
 async function getWeatherData(location) {
-  const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=6275fbc643b73392b948f38f9f200287`,
-    { mode: 'cors' }
-  );
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=6275fbc643b73392b948f38f9f200287`,
+      { mode: 'cors' }
+    );
 
-  return response.json();
+    if (!response.ok) throw new Error('City not found. Please, try again.');
+    return response.json();
+  } catch (err) {
+    alert(err)
+    return;
+  }
 }
 
 export default getWeatherData;
