2b1d35b6485402faf1d34aa7e4f101209af39b0a
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-02-19 19:24:28 +0300

    build search logic

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import getWeatherData from './api_fetcher'
+import getReducedWeatherData from './data';
+import ui from './ui'
+
+/* getWeatherData(prompt('Enter city/country', 'Minsk')).then(getReducedWeatherData).then(console.log); */
+
+async function handleSearchSubmit(inputValue, e) {
+  e.preventDefault();
+
+  let weatherData;
+  let reducedWeatherData;
+
+  try {
+    weatherData = await getWeatherData(inputValue);
+    reducedWeatherData = await getReducedWeatherData(weatherData);
+
+  } catch (err) {
+
+    switch (err.name) {
+      case 'TypeError': {
+        /* Run DOM function with some parameters as 'no data' */
+      }
+      case 'NothingFound': {
+        alert(err.message);
+      }
+    }
+  } finally {
+    // output to DOM
+    console.log(reducedWeatherData);
+  }
+}
+
+ui.initSearch(handleSearchSubmit);
