f6acef4fc6ca82d25fb7abcb5902683a2eb11109
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-02-20 19:44:51 +0300

    fix incorrect units query

--- src/api_fetcher.js
+++ src/api_fetcher.js
@@ -3,7 +3,7 @@
 
 async function getWeatherData(location) {
   const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=6275fbc643b73392b948f38f9f200287`,
+    `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=6275fbc643b73392b948f38f9f200287`,
     { mode: 'cors' }
   );
 
