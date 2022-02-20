1c997cdd7500c2d7810e2b57f15bb42ac81d8c22
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-02-20 19:25:55 +0300

    store displayed data in ui

--- src/ui.js
+++ src/ui.js
@@ -1,9 +1,17 @@
 const ui = (() => {
+  let currentUnits = 'metric'; // default
+  let displayedData;
+
   /* Init */
 
   function initSearch(handleSearchSubmit) {
     const search = document.querySelector('.js-search');
     search.addEventListener('submit', (e) => { handleSearchSubmit(_getInputValue(), e) });
+  }
+
+  function initUnitToggle() {
+    const toggleBtn = document.querySelector('.js-toggle-units');
+    toggleBtn.addEventListener('click', () => _handleUnitsToggle);
   }
 
   function toggleLoading() {
@@ -12,12 +20,31 @@ const ui = (() => {
 
   /* Output */
 
-  function displayResults(data) {
-    document.querySelector('.js-temp').textContent = data['temp'];
-    document.querySelector('.js-location').textContent = `${data['name']}, ${data['country']}`;
-    document.querySelector('.js-feels-like').textContent = data['feels_like'];
-    document.querySelector('.js-humidity').textContent = data['humidity'];
-    document.querySelector('.js-wind-speed').textContent = data['wind_speed'];
+  function handleSearchOutput(data) {
+    displayedData = data;
+    _displayResults(displayedData);
+  }
+
+  function _displayResults() {
+    document.querySelector('.js-location').textContent = `${displayedData['name']}, ${displayedData['country']}`;
+    document.querySelector('.js-humidity').textContent = displayedData['humidity'];
+    document.querySelector('.js-wind-speed').textContent = displayedData['wind_speed'];
+    document.querySelector('.js-temp').textContent = displayedData[`temp_${currentUnits}`];
+    document.querySelector('.js-feels-like').textContent = displayedData[`feels_like_${currentUnits}`];
+    _showUnits(currentUnits === 'metric' ? '\xB0C' : '\xB0F');
+  }
+
+
+  function _showUnits(units) {
+    const unitsElements = document.querySelectorAll('.js-unit');
+
+    unitsElements.forEach(el => {
+      el.textContent = units;
+    });
+  }
+
+  function _handleUnitsToggle() {
+
   }
 
   function _getInputValue() {
@@ -26,8 +53,9 @@ const ui = (() => {
 
   return {
     initSearch,
+    initUnitToggle,
     toggleLoading,
-    displayResults
+    handleSearchOutput,
   };
 })();
 
